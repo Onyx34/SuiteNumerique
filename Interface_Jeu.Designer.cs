c6c78b43cc026f92a3fa1e16b0b3ed4357bbdf64
Onyx34 <fred.terral34@gmail.com>
2024-11-07 21:10:00 +0100

    Ajout de interface_Jeu

---
+++ Interface_Jeu.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace Suites_Numériques
+{
+    partial class Interface_Jeu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label_enonce = new System.Windows.Forms.Label();
+            this.label_element_1 = new System.Windows.Forms.Label();
+            this.label_elment_2 = new System.Windows.Forms.Label();
+            this.label_question = new System.Windows.Forms.Label();
+            this.label_reponse_1 = new System.Windows.Forms.Label();
+            this.label_reponse_2 = new System.Windows.Forms.Label();
+            this.textBox_reponse_1 = new System.Windows.Forms.TextBox();
+            this.textBox_reponse_2 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioBtn_Décroissante = new System.Windows.Forms.RadioButton();
+            this.radioBtn_Constante = new System.Windows.Forms.RadioButton();
+            this.radioBtn_Croissante = new System.Windows.Forms.RadioButton();
+            this.radioBtn_NonMonotone = new System.Windows.Forms.RadioButton();
+            this.label_information = new System.Windows.Forms.Label();
+            this.label_check_1 = new System.Windows.Forms.Label();
+            this.label_check_2 = new System.Windows.Forms.Label();
+            this.label_check_3 = new System.Windows.Forms.Label();
+            this.label_check_4 = new System.Windows.Forms.Label();
+            this.label_check_5 = new System.Windows.Forms.Label();
+            this.label_check_6 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label_enonce
+            // 
+            this.label_enonce.AutoSize = true;
+            this.label_enonce.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_enonce.Location = new System.Drawing.Point(12, 18);
+            this.label_enonce.Name = "label_enonce";
+            this.label_enonce.Size = new System.Drawing.Size(104, 20);
+            this.label_enonce.TabIndex = 0;
+            this.label_enonce.Text = "label_enonce";
+            // 
+            // label_element_1
+            // 
+            this.label_element_1.AutoSize = true;
+            this.label_element_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_element_1.Location = new System.Drawing.Point(37, 60);
+            this.label_element_1.Name = "label_element_1";
+            this.label_element_1.Size = new System.Drawing.Size(122, 20);
+            this.label_element_1.TabIndex = 1;
+            this.label_element_1.Text = "label_enonce_1";
+            // 
+            // label_elment_2
+            // 
+            this.label_elment_2.AutoSize = true;
+            this.label_elment_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_elment_2.Location = new System.Drawing.Point(37, 93);
+            this.label_elment_2.Name = "label_elment_2";
+            this.label_elment_2.Size = new System.Drawing.Size(122, 20);
+            this.label_elment_2.TabIndex = 2;
+            this.label_elment_2.Text = "label_enonce_2";
+            // 
+            // label_question
+            // 
+            this.label_question.AutoSize = true;
+            this.label_question.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_question.Location = new System.Drawing.Point(12, 200);
+            this.label_question.Name = "label_question";
+            this.label_question.Size = new System.Drawing.Size(112, 20);
+            this.label_question.TabIndex = 3;
+            this.label_question.Text = "label_question";
+            // 
+            // label_reponse_1
+            // 
+            this.label_reponse_1.AutoSize = true;
+            this.label_reponse_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_reponse_1.Location = new System.Drawing.Point(37, 250);
+            this.label_reponse_1.Name = "label_reponse_1";
+            this.label_reponse_1.Size = new System.Drawing.Size(127, 20);
+            this.label_reponse_1.TabIndex = 4;
+            this.label_reponse_1.Text = "label_reponse_1";
+            // 
+            // label_reponse_2
+            // 
+            this.label_reponse_2.AutoSize = true;
+            this.label_reponse_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_reponse_2.Location = new System.Drawing.Point(37, 286);
+            this.label_reponse_2.Name = "label_reponse_2";
+            this.label_reponse_2.Size = new System.Drawing.Size(127, 20);
+            this.label_reponse_2.TabIndex = 5;
+            this.label_reponse_2.Text = "label_reponse_2";
+            // 
+            // textBox_reponse_1
+            // 
+            this.textBox_reponse_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_reponse_1.Location = new System.Drawing.Point(170, 250);
+            this.textBox_reponse_1.Name = "textBox_reponse_1";
+            this.textBox_reponse_1.Size = new System.Drawing.Size(200, 26);
+            this.textBox_reponse_1.TabIndex = 6;
+            // 
+            // textBox_reponse_2
+            // 
+            this.textBox_reponse_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_reponse_2.Location = new System.Drawing.Point(170, 286);
+            this.textBox_reponse_2.Name = "textBox_reponse_2";
+            this.textBox_reponse_2.Size = new System.Drawing.Size(200, 26);
+            this.textBox_reponse_2.TabIndex = 7;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioBtn_Décroissante);
+            this.groupBox1.Controls.Add(this.radioBtn_Constante);
+            this.groupBox1.Controls.Add(this.radioBtn_Croissante);
+            this.groupBox1.Controls.Add(this.radioBtn_NonMonotone);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(41, 333);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(393, 69);
+            this.groupBox1.TabIndex = 39;
+            this.groupBox1.TabStop = false;
+            // 
+            // radioBtn_Décroissante
+            // 
+            this.radioBtn_Décroissante.AutoSize = true;
+            this.radioBtn_Décroissante.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioBtn_Décroissante.Location = new System.Drawing.Point(188, 39);
+            this.radioBtn_Décroissante.Name = "radioBtn_Décroissante";
+            this.radioBtn_Décroissante.Size = new System.Drawing.Size(194, 24);
+            this.radioBtn_Décroissante.TabIndex = 3;
+            this.radioBtn_Décroissante.TabStop = true;
+            this.radioBtn_Décroissante.Text = "Monotone décroissante";
+            this.radioBtn_Décroissante.UseVisualStyleBackColor = true;
+            // 
+            // radioBtn_Constante
+            // 
+            this.radioBtn_Constante.AutoSize = true;
+            this.radioBtn_Constante.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioBtn_Constante.Location = new System.Drawing.Point(188, 14);
+            this.radioBtn_Constante.Name = "radioBtn_Constante";
+            this.radioBtn_Constante.Size = new System.Drawing.Size(174, 24);
+            this.radioBtn_Constante.TabIndex = 2;
+            this.radioBtn_Constante.Text = "Monotone constante";
+            this.radioBtn_Constante.UseVisualStyleBackColor = true;
+            // 
+            // radioBtn_Croissante
+            // 
+            this.radioBtn_Croissante.AutoSize = true;
+            this.radioBtn_Croissante.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioBtn_Croissante.Location = new System.Drawing.Point(6, 39);
+            this.radioBtn_Croissante.Name = "radioBtn_Croissante";
+            this.radioBtn_Croissante.Size = new System.Drawing.Size(176, 24);
+            this.radioBtn_Croissante.TabIndex = 1;
+            this.radioBtn_Croissante.TabStop = true;
+            this.radioBtn_Croissante.Text = "Monotone croissante";
+            this.radioBtn_Croissante.UseVisualStyleBackColor = true;
+            // 
+            // radioBtn_NonMonotone
+            // 
+            this.radioBtn_NonMonotone.AutoSize = true;
+            this.radioBtn_NonMonotone.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioBtn_NonMonotone.Location = new System.Drawing.Point(6, 14);
+            this.radioBtn_NonMonotone.Name = "radioBtn_NonMonotone";
+            this.radioBtn_NonMonotone.Size = new System.Drawing.Size(132, 24);
+            this.radioBtn_NonMonotone.TabIndex = 0;
+            this.radioBtn_NonMonotone.TabStop = true;
+            this.radioBtn_NonMonotone.Text = "Non monotone";
+            this.radioBtn_NonMonotone.UseVisualStyleBackColor = true;
+            // 
+            // label_information
+            // 
+            this.label_information.BackColor = System.Drawing.SystemColors.Info;
+            this.label_information.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_information.Location = new System.Drawing.Point(510, 71);
+            this.label_information.Name = "label_information";
+            this.label_information.Size = new System.Drawing.Size(235, 277);
+            this.label_information.TabIndex = 40;
+            this.label_information.Text = "label_information";
+            // 
+            // label_check_1
+            // 
+            this.label_check_1.AutoSize = true;
+            this.label_check_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_check_1.Location = new System.Drawing.Point(508, 27);
+            this.label_check_1.Name = "label_check_1";
+            this.label_check_1.Size = new System.Drawing.Size(32, 31);
+            this.label_check_1.TabIndex = 41;
+            this.label_check_1.Text = "X";
+            // 
+            // label_check_2
+            // 
+            this.label_check_2.AutoSize = true;
+            this.label_check_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_check_2.Location = new System.Drawing.Point(549, 27);
+            this.label_check_2.Name = "label_check_2";
+            this.label_check_2.Size = new System.Drawing.Size(32, 31);
+            this.label_check_2.TabIndex = 42;
+            this.label_check_2.Text = "X";
+            // 
+            // label_check_3
+            // 
+            this.label_check_3.AutoSize = true;
+            this.label_check_3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_check_3.Location = new System.Drawing.Point(590, 27);
+            this.label_check_3.Name = "label_check_3";
+            this.label_check_3.Size = new System.Drawing.Size(32, 31);
+            this.label_check_3.TabIndex = 43;
+            this.label_check_3.Text = "X";
+            // 
+            // label_check_4
+            // 
+            this.label_check_4.AutoSize = true;
+            this.label_check_4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_check_4.Location = new System.Drawing.Point(631, 27);
+            this.label_check_4.Name = "label_check_4";
+            this.label_check_4.Size = new System.Drawing.Size(32, 31);
+            this.label_check_4.TabIndex = 44;
+            this.label_check_4.Text = "X";
+            // 
+            // label_check_5
+            // 
+            this.label_check_5.AutoSize = true;
+            this.label_check_5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_check_5.Location = new System.Drawing.Point(672, 27);
+            this.label_check_5.Name = "label_check_5";
+            this.label_check_5.Size = new System.Drawing.Size(32, 31);
+            this.label_check_5.TabIndex = 45;
+            this.label_check_5.Text = "X";
+            // 
+            // label_check_6
+            // 
+            this.label_check_6.AutoSize = true;
+            this.label_check_6.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_check_6.Location = new System.Drawing.Point(713, 27);
+            this.label_check_6.Name = "label_check_6";
+            this.label_check_6.Size = new System.Drawing.Size(32, 31);
+            this.label_check_6.TabIndex = 46;
+            this.label_check_6.Text = "X";
+            // 
+            // Interface_Jeu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label_check_6);
+            this.Controls.Add(this.label_check_5);
+            this.Controls.Add(this.label_check_4);
+            this.Controls.Add(this.label_check_3);
+            this.Controls.Add(this.label_check_2);
+            this.Controls.Add(this.label_check_1);
+            this.Controls.Add(this.label_information);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.textBox_reponse_2);
+            this.Controls.Add(this.textBox_reponse_1);
+            this.Controls.Add(this.label_reponse_2);
+            this.Controls.Add(this.label_reponse_1);
+            this.Controls.Add(this.label_question);
+            this.Controls.Add(this.label_elment_2);
+            this.Controls.Add(this.label_element_1);
+            this.Controls.Add(this.label_enonce);
+            this.Name = "Interface_Jeu";
+            this.Text = "Interface_Jeu";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label_enonce;
+        private System.Windows.Forms.Label label_element_1;
+        private System.Windows.Forms.Label label_elment_2;
+        private System.Windows.Forms.Label label_question;
+        private System.Windows.Forms.Label label_reponse_1;
+        private System.Windows.Forms.Label label_reponse_2;
+        private System.Windows.Forms.TextBox textBox_reponse_1;
+        private System.Windows.Forms.TextBox textBox_reponse_2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioBtn_Décroissante;
+        private System.Windows.Forms.RadioButton radioBtn_Constante;
+        private System.Windows.Forms.RadioButton radioBtn_Croissante;
+        private System.Windows.Forms.RadioButton radioBtn_NonMonotone;
+        private System.Windows.Forms.Label label_information;
+        private System.Windows.Forms.Label label_check_1;
+        private System.Windows.Forms.Label label_check_2;
+        private System.Windows.Forms.Label label_check_3;
+        private System.Windows.Forms.Label label_check_4;
+        private System.Windows.Forms.Label label_check_5;
+        private System.Windows.Forms.Label label_check_6;
+    }
+}
