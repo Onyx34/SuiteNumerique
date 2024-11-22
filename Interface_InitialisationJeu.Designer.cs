a3c058ab59fb2ab50ef6c2235826b4956e593249
Onyx34 <fred.terral34@gmail.com>
2024-11-22 14:38:02 +0100

    Création Interface Initialisation jeu

---
+++ Interface_InitialisationJeu.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace Suites_Numériques
+{
+    partial class Interface_InitialisationJeu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox_pseudo1 = new System.Windows.Forms.TextBox();
+            this.textBox_pseudo2 = new System.Windows.Forms.TextBox();
+            this.textBox_mdp1 = new System.Windows.Forms.TextBox();
+            this.textBox_mdp2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioBtn_geometrique = new System.Windows.Forms.RadioButton();
+            this.radioBtn_arithmetique = new System.Windows.Forms.RadioButton();
+            this.btn_ok = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(313, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Veuillez saisir les informations des joueurs :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(38, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Joueur N°1 :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(38, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Joueur N°2 :";
+            // 
+            // textBox_pseudo1
+            // 
+            this.textBox_pseudo1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_pseudo1.Location = new System.Drawing.Point(139, 78);
+            this.textBox_pseudo1.Name = "textBox_pseudo1";
+            this.textBox_pseudo1.Size = new System.Drawing.Size(244, 26);
+            this.textBox_pseudo1.TabIndex = 3;
+            // 
+            // textBox_pseudo2
+            // 
+            this.textBox_pseudo2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_pseudo2.Location = new System.Drawing.Point(139, 119);
+            this.textBox_pseudo2.Name = "textBox_pseudo2";
+            this.textBox_pseudo2.Size = new System.Drawing.Size(244, 26);
+            this.textBox_pseudo2.TabIndex = 4;
+            // 
+            // textBox_mdp1
+            // 
+            this.textBox_mdp1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_mdp1.Location = new System.Drawing.Point(508, 78);
+            this.textBox_mdp1.Name = "textBox_mdp1";
+            this.textBox_mdp1.Size = new System.Drawing.Size(284, 26);
+            this.textBox_mdp1.TabIndex = 5;
+            this.textBox_mdp1.UseSystemPasswordChar = true;
+            // 
+            // textBox_mdp2
+            // 
+            this.textBox_mdp2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_mdp2.Location = new System.Drawing.Point(508, 116);
+            this.textBox_mdp2.Name = "textBox_mdp2";
+            this.textBox_mdp2.Size = new System.Drawing.Size(284, 26);
+            this.textBox_mdp2.TabIndex = 6;
+            this.textBox_mdp2.UseSystemPasswordChar = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(389, 81);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(113, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Mot de passe :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(389, 119);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(113, 20);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Mot de passe :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(178, 217);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(263, 20);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Séléctionner le type de suite du jeu :";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioBtn_geometrique);
+            this.groupBox1.Controls.Add(this.radioBtn_arithmetique);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(202, 240);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(369, 69);
+            this.groupBox1.TabIndex = 36;
+            this.groupBox1.TabStop = false;
+            // 
+            // radioBtn_geometrique
+            // 
+            this.radioBtn_geometrique.AutoSize = true;
+            this.radioBtn_geometrique.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioBtn_geometrique.Location = new System.Drawing.Point(219, 25);
+            this.radioBtn_geometrique.Name = "radioBtn_geometrique";
+            this.radioBtn_geometrique.Size = new System.Drawing.Size(120, 24);
+            this.radioBtn_geometrique.TabIndex = 2;
+            this.radioBtn_geometrique.Text = "Géométrique";
+            this.radioBtn_geometrique.UseVisualStyleBackColor = true;
+            // 
+            // radioBtn_arithmetique
+            // 
+            this.radioBtn_arithmetique.AutoSize = true;
+            this.radioBtn_arithmetique.Checked = true;
+            this.radioBtn_arithmetique.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioBtn_arithmetique.Location = new System.Drawing.Point(24, 25);
+            this.radioBtn_arithmetique.Name = "radioBtn_arithmetique";
+            this.radioBtn_arithmetique.Size = new System.Drawing.Size(117, 24);
+            this.radioBtn_arithmetique.TabIndex = 0;
+            this.radioBtn_arithmetique.TabStop = true;
+            this.radioBtn_arithmetique.Text = "Arithmétique";
+            this.radioBtn_arithmetique.UseVisualStyleBackColor = true;
+            // 
+            // btn_ok
+            // 
+            this.btn_ok.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_ok.Location = new System.Drawing.Point(330, 351);
+            this.btn_ok.Name = "btn_ok";
+            this.btn_ok.Size = new System.Drawing.Size(111, 41);
+            this.btn_ok.TabIndex = 37;
+            this.btn_ok.Text = "OK";
+            this.btn_ok.UseVisualStyleBackColor = true;
+            // 
+            // Interface_InitialisationJeu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_ok);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox_mdp2);
+            this.Controls.Add(this.textBox_mdp1);
+            this.Controls.Add(this.textBox_pseudo2);
+            this.Controls.Add(this.textBox_pseudo1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Interface_InitialisationJeu";
+            this.Text = "Interface_InitialisationJeu";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox_pseudo1;
+        private System.Windows.Forms.TextBox textBox_pseudo2;
+        private System.Windows.Forms.TextBox textBox_mdp1;
+        private System.Windows.Forms.TextBox textBox_mdp2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioBtn_geometrique;
+        private System.Windows.Forms.RadioButton radioBtn_arithmetique;
+        private System.Windows.Forms.Button btn_ok;
+    }
+}
