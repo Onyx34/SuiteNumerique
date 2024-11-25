a4f6ffe83aa4eca14918638f14ed44ecda7803d6
Onyx34 <fred.terral34@gmail.com>
2024-11-25 21:49:16 +0100

    Correction du texte de l'élément `toolStrip_SansContrainte`
    Le texte de l'élément `toolStrip_SansContrainte` a été modifié de "Sans Contrainte" à "Sans contrainte".

--- Interface_Acceuil.Designer.cs
+++ Interface_Acceuil.Designer.cs
@@ -98,7 +98,7 @@
             // 
             this.toolStrip_SansContrainte.Name = "toolStrip_SansContrainte";
             this.toolStrip_SansContrainte.Size = new System.Drawing.Size(209, 22);
-            this.toolStrip_SansContrainte.Text = "Sans Contrainte";
+            this.toolStrip_SansContrainte.Text = "Sans contrainte";
             this.toolStrip_SansContrainte.Click += new System.EventHandler(this.toolStrip_SansContrainte_Click);
             // 
             // toolStrip_AvecContrainte
